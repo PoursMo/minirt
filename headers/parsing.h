31fd13676823aa10b62a33210ceb8d9d1a79f353
Amaury <loubry.amaury@gmail.com>
2025-02-22 20:39:12 +0100

    norm

--- headers/parsing.h
+++ headers/parsing.h
@@ -6,24 +6,24 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:18:48 by aloubry           #+#    #+#             */
-/*   Updated: 2025/02/22 20:34:10 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/02/22 20:38:28 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
-#define PARSING_H
+# define PARSING_H
 
 // parsing.c
 
-int parse_file(char *file);
+int		parse_file(char *file);
 
 // parsing_utils.c
 
-float mrt_atof(char **nptr);
-int	mrt_atoi(char **nptr);
-int	is_dot_rt_file(char *file);
-int	is_valid_parameter(char **line);
-int is_valid_tail(char *line);
+float	mrt_atof(char **nptr);
+int		mrt_atoi(char **nptr);
+int		is_dot_rt_file(char *file);
+int		is_valid_parameter(char **line);
+int		is_valid_tail(char *line);
 
 // parsing_utils2.c
 
@@ -31,22 +31,22 @@ void	misconfiguration_error(char *error);
 
 // parsing_shape_getters.c
 
-int	get_sphere(char *line);
-int	get_plane(char *line);
-int	get_cylinder(char *line);
+int		get_sphere(char *line);
+int		get_plane(char *line);
+int		get_cylinder(char *line);
 
 // parsing_type_getters.c
 
-int	parsing_get_float(char **line, float *target, float min, float max);
-int	parsing_get_int(char **line, int *target, int min, int max);
-int	parsing_get_color(char **line, t_color *target);
-int	parsing_get_vector3(char **line, t_vector3 *target);
-int	parsing_get_normalized_vector3(char **line, t_vector3 *target);
+int		parsing_get_float(char **line, float *target, float min, float max);
+int		parsing_get_int(char **line, int *target, int min, int max);
+int		parsing_get_color(char **line, t_color *target);
+int		parsing_get_vector3(char **line, t_vector3 *target);
+int		parsing_get_normalized_vector3(char **line, t_vector3 *target);
 
 // parsing_unique_getters.c
 
-int	get_ambiant_lighting(char *line);
-int	get_camera(char *line);
-int	get_light(char *line);
+int		get_ambiant_lighting(char *line);
+int		get_camera(char *line);
+int		get_light(char *line);
 
 #endif
--- headers/utils.h
+++ headers/utils.h
@@ -6,22 +6,22 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:48:25 by aloubry           #+#    #+#             */
-/*   Updated: 2025/02/22 16:14:51 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/02/22 20:38:57 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
-#define UTILS_H
+# define UTILS_H
 
 // color.c
 
-int rgb_to_int(unsigned char red, unsigned char green, unsigned char blue);
-int t_color_to_int(t_color color);
+int			rgb_to_int(unsigned char red, unsigned char green, unsigned char blue);
+int			t_color_to_int(t_color color);
 
 // scene.c
 
-t_scene *get_scene();
-void free_scene();
+t_scene		*get_scene(void);
+void		free_scene(void);
 
 // vectors.c
 
@@ -33,7 +33,7 @@ t_vector3	v3_normalize(t_vector3 v);
 
 // vectors2.c
 
-double	v3_get_magnitude(t_vector3 v);
-float	v3_dot(t_vector3 a, t_vector3 b);
+double		v3_get_magnitude(t_vector3 v);
+float		v3_dot(t_vector3 a, t_vector3 b);
 
 #endif
