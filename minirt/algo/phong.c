17535a672e5307465d29db20e0f4441b4aca5a15
Amaury <loubry.amaury@gmail.com>
2025-02-28 18:25:24 +0100

    add textures

--- minirt/algo/phong.c
+++ minirt/algo/phong.c
@@ -6,7 +6,7 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 22:00:26 by aloubry           #+#    #+#             */
-/*   Updated: 2025/02/28 14:34:02 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/02/28 18:05:28 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,13 +55,42 @@ int is_ray_colliding(t_ray *ray, t_list *shapes)
 	return (0);
 }
 
+typedef struct s_vector2
+{
+	float x;
+	float y;
+}	t_vector2;
+
+t_vector2 compute_sphere_uv(t_vector3 normal) {
+    float theta = acos(normal.y); // Polar angle
+    float phi = atan2(normal.z, normal.x); // Azimuthal angle
+
+    // Convert to texture coordinates
+    float u = (phi + MRT_PI) / (2.0 * MRT_PI);
+    float v = theta / MRT_PI;
+    return (t_vector2){u, v};
+}
+
+t_color sample_texture(t_vector2 uv, t_img *texture)
+{
+	t_color color;
+    int x = (int)(uv.x * texture->width) % texture->width;
+    int y = (int)(uv.y * texture->height) % texture->height;
+    color = get_pixel_from_mlx_img(texture, x, y);
+	return (color);
+}
+
 // precompute lights intensities ?
 t_color	apply_phong(t_scene *scene, t_ray_hit_info *hit_info)
 {
 	t_color final_color;
 	t_ray	light_ray;
+	t_color point_base_color;
 
-	final_color = calculate_ambient(&hit_info->shape->color, scene);
+	point_base_color = hit_info->shape->color;
+	if (hit_info->shape->texture && hit_info->shape->type == SPHERE)
+		point_base_color = color_add(hit_info->shape->color, sample_texture(compute_sphere_uv(hit_info->normal), hit_info->shape->texture));
+	final_color = calculate_ambient(&point_base_color, scene);
 	light_ray.origin = v3_add(hit_info->position, v3_scale(hit_info->normal, 1e-4));
 	t_list *light_list = scene->lights;
 	while (light_list)
@@ -70,7 +99,7 @@ t_color	apply_phong(t_scene *scene, t_ray_hit_info *hit_info)
 		light_ray.direction = v3_normalize(v3_subtract(light->position, light_ray.origin));
 		if (!is_ray_colliding(&light_ray, scene->shapes))
 		{
-			final_color = color_add(final_color, color_add(calculate_diffuse(&light_ray, hit_info, &hit_info->shape->color, light), calculate_specular(&light_ray, hit_info, light, scene)));
+			final_color = color_add(final_color, color_add(calculate_diffuse(&light_ray, hit_info, &point_base_color, light), calculate_specular(&light_ray, hit_info, light, scene)));
 		}
 		light_list = light_list->next;
 	}
