b8ca3c6b3064fafc63ab89018b7363ea4d4ef0aa
LPittet1 <pittet82@gmail.com>
2025-03-10 11:05:04 +0100

    fixed cone normal

--- minirt/algo/normal.c
+++ minirt/algo/normal.c
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 15:39:42 by aloubry           #+#    #+#             */
-/*   Updated: 2025/03/10 09:59:32 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/03/10 11:02:33 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ static t_vector3 cone_normal(t_shape *shape, t_vector3 point)
 	axis_component = v3_scale(shape->data.cone.axis, 
 								v3_dot(point_to_apex, shape->data.cone.axis));
 	normal = v3_subtract(point_to_apex, axis_component);
-	return (v3_normalize(normal));
+	return (v3_scale(v3_normalize(normal), -1));
 }
 
 t_vector3	get_normal(t_shape *shape, t_vector3 point, t_ray *ray)
