304a44158615e789fce997b90b17fb6721e33ad4
Amaury Loubry <aloubry@c2r4s3.42lausanne.ch>
2025-03-14 13:52:51 +0100

    small cone normal fix

--- minirt/algo/normal.c
+++ minirt/algo/normal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   normal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 15:39:42 by aloubry           #+#    #+#             */
-/*   Updated: 2025/03/14 12:46:34 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/03/14 13:07:10 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,22 +82,22 @@ static t_vector3	cone_normal(t_shape *shape, t_vector3 point)
 	t_vector3	normal;
 	t_vector3	apex;
 	t_vector3	point_to_apex;
-	t_vector3	axis_component;
+	t_vector3	plane_hit_axis;
 	float		dot_hit;
 
-	dot_hit = v3_dot(v3_subtract(shape->data.cone.position, point),
+	dot_hit = v3_dot(v3_subtract(point, shape->data.cone.position),
 			shape->data.cone.axis);
-	if (fabs(dot_hit) < EPSILON)
+	if (fabs(dot_hit) < EPSILON && dot_hit > 0)
 		return (v3_scale(shape->data.cone.axis, -1));
 	apex = v3_add(shape->data.cone.position,
 			v3_scale(shape->data.cone.axis, shape->data.cone.height));
 	point_to_apex = v3_subtract(apex, point);
 	if (v3_get_magnitude(point_to_apex) < EPSILON)
 		return (shape->data.cone.axis);
-	axis_component = v3_scale(shape->data.cone.axis,
-			v3_dot(point_to_apex, shape->data.cone.axis));
-	normal = v3_subtract(point_to_apex, axis_component);
-	return (v3_scale(v3_normalize(normal), -1));
+	plane_hit_axis = v3_cross(shape->data.cone.axis,
+			v3_subtract(point, shape->data.cone.position));
+	normal = v3_cross(plane_hit_axis, shape->data.cone.axis);
+	return (v3_normalize(normal));
 }
 
 t_vector3	get_normal(t_shape *shape, t_vector3 point, t_ray *ray)
